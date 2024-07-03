c1bd6e1a09028f020a78aaf4222d6d235bf8e4d2
iLinx007 <ahiadulincoln@gmail.com>
2024-07-03 14:54:40 +0400

    done

--- lib/home.dart
+++ lib/home.dart
@@ -148,46 +148,42 @@ class _HomeScreenState extends State<HomeScreen> {
         child: BlocBuilder<TodoBloc, TodoState>(builder: (context, state) {
           if (state.status == TodoStatus.success) {
             return ListView.builder(
-              itemCount: state.todos.length,
-              itemBuilder: (context, int i){
-                return Card(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  elevation: 1,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Slidable(
-                    key: const ValueKey(0),
-                    startActionPane: ActionPane(
-                      motion: const ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          onPressed: (_) {
-                            removeTodo(state.todos[i]);
+                itemCount: state.todos.length,
+                itemBuilder: (context, int i) {
+                  return Card(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    elevation: 1,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Slidable(
+                      key: const ValueKey(0),
+                      startActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            onPressed: (_) {
+                              removeTodo(state.todos[i]);
+                            },
+                            backgroundColor: const Color(0xFFFE4A49),
+                            foregroundColor: Colors.white,
+                            icon: Icons.delete,
+                            label: 'Delete',
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        title: Text(state.todos[i].title),
+                        trailing: Checkbox(
+                          value: state.todos[i].isDone,
+                          activeColor: Theme.of(context).colorScheme.onPrimary,
+                          onChanged: (value) {
+                            alterTodo(i);
                           },
-                          backgroundColor: const Color(0xFFFE4A49),
-                          foregroundColor:  Colors.white,
-                          icon: Icons.delete,
-                          label: 'Delete',
                         ),
-                      ],
-                    ),
-                    child: ListTile(
-                      title: Text(
-                        state.todos[i].title
-                      ),
-                      trailing: Checkbox(
-                        value: state.todos[i].isDone,
-                        activeColor: Theme.of(context).colorScheme.onPrimary,
-                        onChanged: (value) {
-                          alterTodo(i);
-                        },
                       ),
                     ),
-                  ),
-                )
-              }
-              );
+                  );
+                });
           } else if (state.status == TodoStatus.initial) {
             return const Center(child: CircularProgressIndicator());
           } else {
